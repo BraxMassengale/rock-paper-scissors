93d5a48bbc0795722abd8285356f8c3e398e9da7
Braxton <Braxtonmassengale@outlook.com>
2020-04-06 17:33:24 +0200

    update

--- script.js
+++ script.js
@@ -29,44 +29,44 @@ let playRound = function(playerInput = prompt("Do you choose Rock, Paper, or Sci
     computerChoice = computerPlay(getChoice());
     if (playerInput.toLowerCase() === "rock" || playerInput.toLowerCase() === "paper" || playerInput.toLowerCase() === "scissors") {
         playerChoice = playerInput;
-        console.log("The player choice is " + playerChoice + " and the computer's choice is " + computerChoice);
+        alert("The player choice is " + playerChoice + " and the computer's choice is " + computerChoice);
         if (playerChoice === computerChoice) {
-            console.log("It's a tie!")
+            alert("You and the computer tied this round");
             return 0;
         }
 
         else if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "scissors" && computerChoice === "rock")) {
-            console.log("You lose!")
+            alert("You lost this round");
             return 1;
         }
         
         else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-            console.log("You win!")
+            alert("You won this round");
             return 2;
         }
     }
     else {
-        console.log("Error: please enter either rock, paper, or scissors.")   
+        alert("Error: please enter either rock, paper, or scissors.")   
     }
 }
 
 let game = function() {
-    let playerWins = 0;
+    let playerWins = 0; //Perhaps a while loop would be better
     let computerWins = 0;
-    
+    alert("Alright, let's play Rock, Paper, Scissors!");
     for(let i = 0; i < 5; i++) {
         if (playerWins !== 3 || computerWins !== 3) {
             switch (playRound()) {
                 case 0:
-                    console.log("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
+                    alert("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
                     break;
                 case 1:
                     computerWins++;
-                    console.log("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
+                    alert("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
                     break;
                 case 2:
                     playerWins++;
-                    console.log("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
+                    alert("The computer's score is " + computerWins + " points. " + name + "'s score is " + playerWins + " points");
                     break;
             }
         }
@@ -74,17 +74,21 @@ let game = function() {
 
 
     if (playerWins === computerWins) {
+        alert("The game was a tie!"); 
         return "The game was a tie!";
     }
 
     else if (playerWins > computerWins) {
+        alert("You won the game!");
         return "You won the game!";
     }
 
     else if (playerWins < computerWins) {
+        alert("You lost the game!");
         return "You lost the game!";
     }
 
 
     
 }
+
