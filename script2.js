17e2817f3be0f46cce8bfa23842b1a80d0a258b6
Braxton <Braxtonmassengale@outlook.com>
2020-04-08 17:15:42 +0200

    update

--- script2.js
+++ script2.js
@@ -6,6 +6,12 @@ const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const computer_score_p = document.getElementById("computer-score");
 const player_score_p = document.getElementById("player-score");
+const computer_choice_banner_p = document.getElementById("computer-choice-banner");
+const player_choice_banner_p = document.getElementById("player-choice-banner");
+const how_to_play = document.getElementById("how-to-play");
+const how_to_play_p1 = document.getElementById("how-to-play-p1");
+const how_to_play_p2 = document.getElementById("how-to-play-p2");
+
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -13,36 +19,52 @@ function getComputerChoice() {
     return choices[randomNum];
 }
 
-function playerWins() {
+function playerWins(player, computer) {
     playerScore++;
     player_score_p.innerHTML = playerScore;
+    computer_score_p.innerHTML = computerScore;
+    how_to_play.innerHTML = "Boom! " + player + " beats " + computer;
+    how_to_play_p1.innerHTML = "";
+    how_to_play_p2.innerHTML = "";
 }
 
-function playerLoses() {
+function playerLoses(player, computer) {
+    computerScore++;
+    player_score_p.innerHTML = playerScore;
+    computer_score_p.innerHTML = computerScore;
+    how_to_play.innerHTML = "Aw, too bad! " + player + " loses to " + computer;
+    how_to_play_p1.innerHTML = "";
+    how_to_play_p2.innerHTML = "";
 }
-function playerTies() {
+function playerTies(player, computer) {
+    player_score_p.innerHTML = playerScore;
+    computer_score_p.innerHTML = computerScore;
+    how_to_play.innerHTML = "Well, " + player + " draws with " + computer;
+    how_to_play_p1.innerHTML = "";
+    how_to_play_p2.innerHTML = "";
 }
 
 function game(playerInput) {
     const computerInput = getComputerChoice();
-    //alert(computerInput)
     switch (playerInput + computerInput) {
         case "rockscissors":
         case "scissorspaper":
         case "paperrock":
-            playerWins();
+            playerWins(playerInput, computerInput);
             break;
         case "rockpaper":
         case "scissorsrock":
         case "paperscissors":
-            playerLoses();
+            playerLoses(playerInput, computerInput);
             break;
         case "rockrock":
         case "scissorsscissors":
         case "paperpaper":
-            playerTies();
+            playerTies(playerInput, computerInput);
             break;
     }
+    player_choice_banner_p.innerHTML = "You chose " + playerInput;
+    computer_choice_banner_p.innerHTML = "The computer chose " + computerInput;
 }
 
 function main() {
@@ -60,7 +82,6 @@ function main() {
 }
 
 main();
-
 
 
 
